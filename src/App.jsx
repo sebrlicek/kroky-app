acf7d3b16ae19182c87962e0a34c5ccd69d391e7
sebrlicek <sebrlicek@gmail.com>
2025-10-15 17:37:36 +0200

    off.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function Maintenance() {
   return (
     <div style={{ textAlign: 'center', marginTop: '20%' }}>
-      <h1>Web je momentálně nedostupný</h1>
+      <h2>Web je momentálně vypnuty nacti mne pres https://github.com/sebrlicek/kroky-app/edit/main/src/App.jsx   editem </h2>
       <p>Zkus to prosím později.</p>
     </div>
   );
